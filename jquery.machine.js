ecb2552906e6d67462751729cf1ef596506f764d
Marak Squires <marak.squires@gmail.com>
2010-09-08 01:32:49 -0400

    cleaned up api and added demo.html, which shows basic state machine examples

--- jquery.machine.js
+++ jquery.machine.js
@@ -1,73 +1,72 @@
 // create the machine selector
 
 $.fn.machine = function(settings) {
+  
+  // this is an example of a configuration object
+  // there are four basic states for every machine: entering, entered, exiting, exited
   var config = {
-    state : "default",
-    entering : function(){
-      debug.log('entering state');
+    entering : function( state ){
+      debug.log('entering state ', state);
     },
     entered : function(){
-      debug.log('entered state');
+      debug.log('entered state ', state);
     },
     exiting : function(){
-      debug.log('exiting state');
+      debug.log('exiting state ', state);
     },
     exited : function(){
-      debug.log('exited state');
+      debug.log('exited state ', state);
     }
   };
 
-  //if (settings) $.extend(config, settings);
+  // override default config with passed in settings
   var settings = settings || config;
   
-  
-  debug.log($(this).data('machine') || {});
-  // check if there is already a machine behavior defined, this code is not optimal (see: wrong) and should be replaced with better settings / config
+  // check if there is already a machine behavior defined, this code is not optimal and could be replaced with better settings / config
   if($(this).data('machine') != null){
    debug.log('is already a machine! not going to reapply machine settings');
    return false;  
   }
   
-  debug.log('machine behavior being applied to ', $(this) , settings);
-  
   // attach the machine metadata to DOM selector
-  $(this).data( 'machine' , settings );  
+  $(this).data('machine' , settings );  
   
   // in addition to setting the machine metadata we are going to actually set the data-behaviors tag attribute ( for consistancy / easy parsing )
   $(this).attr('data-behaviors', 'machine');
-        
+  
+  // continue the chain
   return this;
 };
 
 // create the machine itself
-var machine = {};
+var machine = {}; // global namespace is not ideal
+
+// the machine can enter diffirent states in diffirent contextes
 machine.enter = function( state , context ){
-  debug.log('entering state : ', state);
   
-  if(typeof context == 'undefined'){
+  if(typeof context == 'undefined' || context == 'document'){
     var context = document;
   }
-  
-  //debug.log($(context));  
+
+  //debug.log('entering state : ', state, ' with context ', context);
+
   // a new state has been entered, find all elements that are machines and check if they match
   $("[data-behaviors*='machine']", $(context)).each(function(i,e){
     var stateMachine = $(e).data('machine') || false;
     if(stateMachine){
-      debug.log(stateMachine, $(e));
+      //debug.log(stateMachine, $(e));
       //debug.log('the state is ', state);
-      debug.log('about to execute ', stateMachine.entered.toString());
+      //debug.log('about to execute ', stateMachine.entered.toString());
       $(e).data( 'state' , state );  
       stateMachine.entered.apply( this, [state] );
-      //$(e).data('machine');
     }
-    
   });
-  
 };
 
-
+// helper method for getting context, not fully implemented
 machine.getContext = function(machine,context){
-  debug.log('getting context', machine, context);
+  
+  //debug.log('getting context', machine, context);
   
   // determine the context of the view we are in
   var context = $(machine).parents().find("[data-behaviors*='machine']");
